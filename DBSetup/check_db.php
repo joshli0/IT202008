e2f684284ef08b15d20886caa135a4cf0f33bb52
Josh Li <jdl47@njit.edu>
2020-01-28 16:27:01 -0500

    Day 3

--- DBSetup/check_db.php
+++ DBSetup/check_db.php
@@ -4,19 +4,19 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require("config.php");
-echo "DBUser: " \ $dbuser;
+echo "DBUser: " . $dbuser;
 echo "\n\r";
 
-$connection_string = "mysql:host=$dbhost;dbname=$database;charset=utf8mb4";
-
+$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+echo $connection_string;
 try{
-	$db = newPDO($connection_string, $dbuser, $dbpass);
+	$db = new PDO($connection_string, $dbuser, $dbpass);
 	echo "should have connected";
-	$stmt = $db->prepare("CREATE TABLE 'Test' (
-			'id' int auto_increment not null,
-			'username' varchar(30) not null unique,
-			'pin' int default 0,
-			PRIMARY KEY ('id')
+	$stmt = $db->prepare("CREATE TABLE `Test` (
+			`id` int auto_increment not null,
+			`username` varchar(30) not null unique,
+			`pin` int default 0,
+			PRIMARY KEY (`id`)
 			) CHARACTER SET utf8 COLLATE utf8_general_ci"
 		);
 	$stmt->execute();
@@ -24,6 +24,6 @@ try{
 }
 catch(Exception $e){
 	echo $e->getMessage();
-	exit("It didn't work:);
+	exit("It didn't work");
 }
 ?>
---
+++ DBSetup/config.php
@@ -0,0 +1,6 @@
+<?php
+$dbuser = "jdl47";
+$dbpass = "Crazyman4evr!";
+$dbhost = "sql1.njit.edu";
+$dbdatabase = "jdl47";
+?>
